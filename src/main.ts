4008b19c4df4e826fc697fff7b34b84416f14b27
Adrian Kunz <clashsoft@hotmail.com>
2021-11-17 20:13:28 +0100

    feat: Add OpenAPI/Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Story Service')
+    .setDescription('The backend service for https://app.clashsoft.de/stories')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
