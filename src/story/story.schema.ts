4e5c56d2eaf2a6904c527447300521c83ec5c20c
Adrian Kunz <clashsoft@hotmail.com>
2021-11-17 20:46:00 +0100

    feat: Validate story name

--- src/story/story.schema.ts
+++ src/story/story.schema.ts
@@ -1,11 +1,13 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import {ApiProperty} from '@nestjs/swagger';
+import {IsNotEmpty} from 'class-validator';
 import {Document} from 'mongoose';
 
 @Schema()
 export class Story {
   @Prop()
   @ApiProperty()
+  @IsNotEmpty()
   name: string;
 }
 
